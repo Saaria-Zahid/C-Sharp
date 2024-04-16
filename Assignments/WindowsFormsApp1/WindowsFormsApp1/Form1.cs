ec30cd9b494736013c1eabc74d3aa3c096b55e0e
Saaria Zahid <102374986+Saaria-Zahid@users.noreply.github.com>
2024-04-17 00:38:09 +0500

    Add files via upload

---
+++ Assignments/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string dapp = "Data Source=PROSAARIA\\SQLEXPRESS;Initial Catalog=dapp1;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(dapp);
+
+            string name, email, contact;
+            name = textBox1.Text;
+            email = textBox2.Text;
+            contact = textBox3.Text;
+
+            if (name != string.Empty || email != string.Empty || contact != string.Empty)
+            {
+
+                connection.Open();
+
+                SqlCommand command = new SqlCommand();
+                command.Connection = connection;
+                command.CommandType = CommandType.Text;
+                command.CommandText = "insert into info (name , email , contact ) Values('" + name + "', '" + email + "', '" + contact + "' )";
+                command.ExecuteNonQuery();
+                MessageBox.Show("Account Successfully created");
+            }
+            else
+            { MessageBox.Show("Error"); }
+            connection.Close();
+        }
+
+           
+
+          
+        }
+            
+    }
+
+    
+    
---
+++ Assignments/clientform/clientform/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clientform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label17_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog uimg = new OpenFileDialog();
+            uimg.Filter = "Image Files (*.png;*.jpg;*.jpeg;.*.gif;)|*.jpg;*.jpeg;.*.gif";
+            if (uimg.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(uimg.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string dapp = "Data Source=PROSAARIA\\SQLEXPRESS;Initial Catalog=client;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(dapp);
+
+
+            string contactval = "";
+
+            if (radioButton1.Checked)
+            {
+                contactval = "Email";
+            }
+            else
+            {
+                contactval = "Phone";
+            }
+
+            string name, title, company, contact, address, addressline, city, state, postal, country, cweb, hours, days, bill, additional;
+            name = textBox1.Text;
+            title = textBox2.Text;
+            company = textBox3.Text;
+            contact = contactval;
+            address = textBox4.Text;
+            addressline = textBox5.Text;
+            city = textBox6.Text;
+            state = textBox7.Text;
+            postal = textBox8.Text;
+            country = this.comboBox1.Text;
+            cweb = textBox9.Text;
+            //logo = pictureBox1.Image;
+            hours = textBox10.Text;
+            days = textBox11.Text;
+            bill = this.dateTimePicker1.Text;
+            additional = textBox12.Text;
+
+            if (name != string.Empty || title != string.Empty || company != string.Empty || contact != string.Empty || address != string.Empty || addressline != string.Empty || city != string.Empty || postal != string.Empty || country != string.Empty || cweb != string.Empty || hours != string.Empty || days != string.Empty || bill != string.Empty || additional != string.Empty)
+            {
+
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "insert into info (name, title, company, contact, address, addressline, city, state, postal, country, cweb, hours, days, bill, additional) Values('" + name + "','" + title + "','" + company + "','" + contact + "','" + address + "','" + addressline + "','" + city + "','" + state + "','" + postal + "','" + country + "','" + cweb + "','" + hours + "', '" + days + "', '" + bill + "', '" + additional + "')  ";
+                cmd.ExecuteNonQuery();
+
+
+                MessageBox.Show("Your Data Successfully Submitted");
+            }
+
+            else
+            {
+
+                MessageBox.Show("Error Hai");
+            }
+            connection.Close();
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
